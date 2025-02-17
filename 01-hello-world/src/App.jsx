84bf06d78b72a6d9e3c18f3eca2bb3286df5c9ae
Horiyasiddique <horiyasiddique52@gmail.com>
2025-02-17 20:43:52 +0500

    learn react

---
+++ 01-hello-world/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World from Hooriya Siddiqui💖💖</h1>
+      <h4>Let's deep dive into the world of React</h4>
+    </div>
+  )
+}
+
+export default App
